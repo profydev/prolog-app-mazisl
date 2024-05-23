3941e56a5db3bcc812c5ecf67f18ca48c153074a
Maz <maz.islam1@gmail.com>
2024-05-23 10:08:25 +0400

    updated support btn to open email app on click

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -103,7 +103,10 @@ export function SidebarNavigation() {
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              onClick={() =>
+                (window.location.href =
+                  "mailto:support@prolog-app.com?subject=Support%20Request:")
+              }
             />
             <MenuItemButton
               text="Collapse"
