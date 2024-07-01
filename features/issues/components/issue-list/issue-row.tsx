00823ff205101f0fa8c5954c55bb76e86f6afb1c
Maz <maz.islam1@gmail.com>
2024-07-01 20:30:31 +0400

    mobile responsive issues page complete

--- features/issues/components/issue-list/issue-row.tsx
+++ features/issues/components/issue-list/issue-row.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import capitalize from "lodash/capitalize";
 import { Badge, BadgeColor, BadgeSize } from "@features/ui";
 import { ProjectLanguage } from "@api/projects.types";
@@ -45,5 +46,46 @@ export function IssueRow({ projectLanguage, issue }: IssueRowProps) {
       <td className={styles.cell}>{numEvents}</td>
       <td className={styles.cell}>{numUsers}</td>
     </tr>
+  );
+}
+
+export function IssueCard({ projectLanguage, issue }: IssueRowProps) {
+  const { name, message, stack, level, numEvents, numUsers } = issue;
+  const firstLineOfStackTrace = stack.split("\n")[1];
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img
+          className={styles.languageIcon}
+          src={`/icons/${projectLanguage}.svg`}
+          alt={projectLanguage}
+        />
+        <div>
+          <div className={styles.errorTypeAndMessage}>
+            <span className={styles.errorType}>{name}:&nbsp;</span>
+            {message}
+          </div>
+          <div>{firstLineOfStackTrace}</div>
+        </div>
+      </div>
+
+      <div className={styles.cardDetails}>
+        <div className={styles.cardDetail}>
+          <div className={styles.cardDetailHeader}>Level</div>
+          <Badge color={levelColors[level]} size={BadgeSize.sm}>
+            {capitalize(level)}
+          </Badge>
+        </div>
+        <div className={styles.cardDetail}>
+          <div className={styles.cardDetailHeader}>Events</div>
+          <div>{numEvents}</div>
+        </div>
+        <div className={styles.cardDetail}>
+          <div className={styles.cardDetailHeader}>Users</div>
+          <div>{numUsers}</div>
+        </div>
+      </div>
+    </div>
   );
 }
